098578be3dfa3132e095d12cbede0003a8ef1e5f
Muneer Ahmed Syed <muneer@ishtech.fi>
2024-02-15 13:44:13 +0200

    enum for ints

---
+++ src/fi/ishtech/sudoku/solver/enums/IntEnum.java
@@ -0,0 +1,64 @@
+package fi.ishtech.sudoku.solver.enums;
+
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public enum IntEnum {
+
+	// @formatter:off
+	ONE(1),
+	TWO(2),
+	THREE(3),
+	FOUR(4),
+	FIVE(5),
+	SIX(6),
+	SEVEN(7),
+	EIGHT(8),
+	NINE(9);
+	// @formatter:on
+
+	private int value;
+
+	IntEnum(int value) {
+		if (value < 1 || value > 9) {
+			throw new IllegalArgumentException(
+					"Invalid value(" + value + ") for IntEnum. Value must be between 1 and 9 (inclusive).");
+		}
+		this.value = value;
+	}
+
+	public int getValue() {
+		return this.value;
+	}
+
+	private static final Map<Integer, IntEnum> lookup = new HashMap<>();
+
+	static {
+		for (IntEnum e : IntEnum.values()) {
+			lookup.put(e.value, e);
+		}
+	}
+
+	public static IntEnum fromValue(Integer value) {
+		if (value == null) {
+			return null;
+		}
+
+		if (lookup.containsKey(value)) {
+			return lookup.get(value);
+		} else {
+			throw new RuntimeException(
+					"Invalid value(" + value + ") for IntEnum. Value must be between 1 and 9 (inclusive).");
+		}
+	}
+
+	private static final Set<IntEnum> all = Collections.unmodifiableSet(EnumSet.allOf(IntEnum.class));
+
+	public static Set<IntEnum> all() {
+		return all;
+	}
+
+}
